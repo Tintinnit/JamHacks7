b6254f576cdcf0cf9719bfe8400884fa8d733a8e
Tony Liu <tony0818abc@gmail.com>
2023-06-10 09:54:58 -0400

    Added basic functionality to extension

--- gpt.js
+++ gpt.js
@@ -8,17 +8,12 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 // Create an api call to simplify terms and conditions into concise and simple bullet points
-async function simplify(terms) {
-    const response = await openai.createChatCompletion({
+export async function simplify(terms) {
+    return await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [
-            {role:"user", content: "Summarize the key points from the following terms and conditions using concise and simple bullet points with casual vocabulary and phrasing.\n\n" + terms},
+            {role:"user", content: "Simplify the key points from the following terms and conditions using concise and short bullet points and simple vocabulary and phrasing.\n\n" + terms},
         ],
     });
-
-    console.log(response.data.choices[0].message.content);
 }
 
-// Test terms and conditions
-let terms = "You may use the Services only if you agree to form a binding contract with us and are not a person barred from receiving services under the laws of the applicable jurisdiction. In any case, you must be at least 13 years old, or in the case of Periscope 16 years old, to use the Services. If you are accepting these Terms and using the Services on behalf of a company, organization, government, or other legal entity, you represent and warrant that you are authorized to do so and have the authority to bind such entity to these Terms, in which case the words “you” and “your” as used in these Terms shall refer to such entity.";
-simplify(terms);
---
+++ index.js
@@ -0,0 +1,19 @@
+import {simplify} from "./gpt.js";
+import express from "express";
+import cors from "cors";
+import json from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(json());
+
+app.post("/", async (req, res) => {
+    const response = await simplify(req.body.userInput);
+    res.send({body: response.data.choices[0].message.content}); 
+}); 
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
