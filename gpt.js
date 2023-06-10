f73d77098d3b708d66c56115b8a27e5b99cc2978
Grogles <jefferyq229@gmail.com>
2023-06-10 18:57:11 -0400

    updated temperature

--- gpt.js
+++ gpt.js
@@ -12,7 +12,7 @@ export async function simplify(terms) {
     return await openai.createCompletion({
         model: "text-davinci-003",
         prompt: "Select the most important few key points from the following terms that is most likely to affect the user's privacy. It should make the user aware of the potentially dangerous terms they are agreeing while omitting irrelevant statements like introductions or term definitions. Summarize briefly in an overview in point form, omit points with similar meanings, with no extra titles or headings. \nIf the text does not seem to be some sort of terms, conditions, or privacy policy, prompt to try again.\n" + terms,
-        temperature: 0.4,
+        temperature: 0.5,
         max_tokens: 2500,
         top_p: 1,
         frequency_penalty: 0,
