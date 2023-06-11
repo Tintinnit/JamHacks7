0e915388301c7dba1c7260b93611c3032246c322
Tony Liu <tony0818abc@gmail.com>
2023-06-10 21:55:51 -0400

    Added example to GPT API prompt

--- gpt.js
+++ gpt.js
@@ -11,9 +11,9 @@ const openai = new OpenAIApi(configuration);
 export async function simplify(terms) {
     return await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Select the most important few key points from the following terms that is most likely to affect the user's privacy. It should make the user aware of the potentially dangerous terms they are agreeing while omitting irrelevant statements like introductions or term definitions. Summarize briefly in an overview in point form, omit points with similar meanings, with no extra titles or headings. \nIf the text does not seem to be some sort of terms, conditions, or privacy policy, prompt to try again.\n" + terms,
+        prompt: "Select the most important few key points from the following terms that is most likely to affect the user's privacy. It should make the user aware of the potentially dangerous terms they are agreeing while omitting irrelevant statements like introductions or term definitions. Summarize briefly in an overview in point form, omit points with similar meanings, with no extra titles or headings. \nIf the text does not seem to be some sort of terms, conditions, or privacy policy, prompt to try again.\n Example: You may use the Services only if you agree to form a binding contract with us and are not a person barred from receiving services under the laws of the applicable jurisdiction. In any case, you must be at least 13 years old, or in the case of Periscope 16 years old, to use the Services. If you are accepting these Terms and using the Services on behalf of a company, organization, government, or other legal entity, you represent and warrant that you are authorized to do so and have the authority to bind such entity to these Terms, in which case the words “you” and “your” as used in these Terms shall refer to such entity.\nSimplified:\n- You must not be barred from this service by the law. \n- You must be at least 13 years old, or 16 for periscope. \n- If you are using this service for a company, you must ensure the company follows these terms.\n\n" + terms,
         temperature: 0.5,
-        max_tokens: 2500,
+        max_tokens: 750,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
